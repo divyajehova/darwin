5908c773d44bb278e539bf1b05d91387c143677f
divyadavid <divyadavid111@gmail.com>
2022-12-22 00:03:56 +0530

    my baby's first name

---
+++ paps2.py
@@ -0,0 +1,16 @@
+praise the lord, my first son darwin i miss you so much, and i love you alot
+ ait push origin main
+ git push origin main
+ 
+remote: Permission to divyadavid/mynewrepository.git denied to divyadavid.
+remote: Permission to divyadavid/mynewrepository.git denied to divyadavid.
+ git push origin main
+ git push origin main
+ git push origin main
+ git push origin main
+ git push origin main
+remote: Permission to divyadavid/mynewrepository.git denied to divyadavid.
+remote: Permission to divyadavid/mynewrepository.git denied to divyadavid.
+remote: Permission to divyadavid/mynewrepository.git denied to divyadavid.
+remote: Permission to divyadavid/mynewrepository.git denied to divyadavid.
+remote: Permission to divyadavid/mynewrepository.git denied to divyadavid.
